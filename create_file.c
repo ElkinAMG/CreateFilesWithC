e854841104887a4eba84be84fbc1e5ab0f38206a
root <elkinandresmejiagueto@gmail.com>
2020-02-04 21:46:42 -0500

    Adding extension for c

--- create_file.c
+++ create_file.c
@@ -6,8 +6,7 @@ void create(int y){
   FILE *fp;
   char files[31][31];
   char message[100];
-  char chmod[100];
-  int x;
+  char option[100];
   for(int i = 0; i < y; i++){
     printf("Type the name of the file<<< ");
     scanf("%s", files[i]);
@@ -18,10 +17,16 @@ void create(int y){
   }
   system("echo \"you have created these files>>>\"");
   sprintf(message, "ls -t | head -n %i | sort -f", y);
-  for(int a = 0; a < y; a++){
-    sprintf(chmod, "chmod u+x %s", files[a]);
-    system(chmod);
-  }
+  //ADD PERMISSION EXECUTE TO OWNER
+  //for(int a = 0; a < y; a++){
+  //  sprintf(option, "chmod u+x %s", files[a]);
+  //  system(option);
+  // }
+  //ADD EXTENSION .C
+  for(int i = 0; i < y; i++){
+    sprintf(option, "mv %s %s.c", files[i], files[i]);
+    system(option);
+  } 
   system(message);
   }
 
